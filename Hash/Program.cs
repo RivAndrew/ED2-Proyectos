b8252e9c3242cc769f8406c06da6df176eb306b8
RivAndrew <andrew_rivera1000@hotmail.com>
2023-06-27 16:25:15 -0700

    Se agregaron explicaciones en MetodosOrdenamiento y en Hash

--- Hash/Program.cs
+++ Hash/Program.cs
@@ -10,21 +10,27 @@ namespace Hash
     {
         static void Main(string[] args)
         {
-            int size = 4;
+            int size = 4;   // Tamaño de la lista
             List<Lista> grafo = new List<Lista>(new Lista[size]);
 
+            // Usando el ejemplo de la presentacion agrego a una lista de tamaño 5 el valor MUNDO con clave HOLA
+            // que se asignara en el indice 2.
             Dato datoA = new Dato("HOLA", "MUNDO");
             Lista verticeA = new Lista("MUNDO");
             grafo.Insert(verticeA.FuncionHash(datoA.Clave,size), verticeA);
 
+            // Siguiendo con el ejemplo se agrega otro valor con clave IIII que deberia realizar una colision
+            // ya que ocasiona colision este nuevo valor lo relaciono con el que estaba en esa posicion.
             Dato datoB = new Dato("IIII", "NADA");
             Lista verticeB = new Lista("NADA");
 
+            // No sé si deberiamos haber hecho un metodo que detectara colisiones, asi que ocasione una colision.
             if (verticeB.FuncionHash(datoB.Clave, size) == verticeA.FuncionHash(datoA.Clave, size)) 
             {
                 verticeA.Relacionar(verticeB);
             }
 
+            // Se agrega otro valor con indice en 4
             Dato datoC = new Dato("Estructura", "ED");
             Lista verticeC = new Lista("ED");
             grafo.Insert(verticeC.FuncionHash(datoC.Clave, size), verticeC);
--- MetodosOrdenamiento/Program.cs
+++ MetodosOrdenamiento/Program.cs
@@ -31,17 +31,18 @@ namespace MetodosOrdenamiento
             }
         }
 
+        // BubbleSort es mover los numeros mas grandes a un extremo del arreglo, utilizando comparaciones de numeros vecinos.
         static void BubbleSort(int[] arreglo)
         {
-            for (int i = 0; i < arreglo.Length - 1; i++)
-            {
-                for (int j = 0; j < arreglo.Length - i - 1; j++)    
-                {
-                    if (arreglo[j] > arreglo[j + 1])
+            for (int i = 0; i < arreglo.Length - 1; i++) // se repiten las instrucciones de adentro el numero de elementos del arreglo menos 1 por que estamos comparando 
+            {                                            // con el numero siguiente y no podemos comparar el numero final con el siguiente por que no hay nada despues.
+                for (int j = 0; j < arreglo.Length - i - 1; j++) // se reptien las instrucciones de adentro el numero de elementos del arreglo menos 1 y menos i por que, 
+                {                                                // si se hizo significa que el valor mas grande esta hasta la derecha y no se necesita volver a checar.
+                    if (arreglo[j] > arreglo[j + 1]) // se compara si el valor actual es mayor que el siguiente valor
                     {
-                        int temp = arreglo[j];
-                        arreglo[j] = arreglo[j + 1];
-                        arreglo[j + 1] = temp;
+                        int temp = arreglo[j]; // se guarda el valor mas grande a mover en una variable temporal.
+                        arreglo[j] = arreglo[j + 1]; // se guarda el valor mas chico en el espacio anterior.
+                        arreglo[j + 1] = temp; // se coloca el valor mas grande a la derecha.
                     }
                 }
             }
